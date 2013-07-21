75b670922cca4be13e43836df60fad379e4454a9
Behdad Esfahbod <behdad@google.com>
2013-07-21 09:47:32 +0200

    [graph] Fix BGRA blending.
    * graph/gblbgra.h: Simplify and correct blending operation.

--- graph/gblbgra.h
+++ graph/gblbgra.h
@@ -7,59 +7,50 @@
 
   do
   {
-    const unsigned char*  src = src_line + blit->src_x*4;
-    unsigned char*        dst = dst_line + blit->dst_x*GDST_INCR;
+    const unsigned char*  src = src_line + blit->src_x * 4;
+    unsigned char*        dst = dst_line + blit->dst_x * GDST_INCR;
     int                   w   = blit->width;
 
     do
     {
-      int  a = GBLENDER_SHADE_INDEX(src[3]);
-      int ra = src[3];
-      int b = src[0];
-      int g = src[1];
-      int r = src[2];
+      int  a  = GBLENDER_SHADE_INDEX(src[3]);
+      int  ra = src[3];
+
+      int  b = src[0];
+      int  g = src[1];
+      int  r = src[2];
+
 
       if ( a == 0 )
       {
         /* nothing */
       }
-      else if ( a == GBLENDER_SHADE_COUNT-1 )
+      else if ( a == 255 )
       {
-        GDST_COPY_VAR
-        GDST_COPY(dst);
+        dst[0] = r;
+        dst[1] = g;
+        dst[2] = b;
       }
       else
       {
-        b = b * 255 / ra;
-        g = g * 255 / ra;
-        r = r * 255 / ra;
+        int  ba = 255 - ra;
+        int  br = dst[0];
+        int  bb = dst[1];
+        int  bg = dst[2];
 
-        {
-          GBLENDER_VARS(blender,color);
 
-          GBlenderPixel  back;
-
-          GDST_READ(dst,back);
-
-          GBLENDER_LOOKUP( blender, back );
-
-#ifdef GBLENDER_STORE_BYTES
-          GDST_STOREB(dst,_gcells,a);
-#else
-          GDST_STOREP(dst,_gcells,a);
-#endif
-
-          GBLENDER_CLOSE(blender);
-        }
-     }
+	dst[0] = br * ba / 255 + r;
+	dst[1] = bg * ba / 255 + g;
+	dst[2] = bb * ba / 255 + b;
+      }
 
       src += 4;
       dst += GDST_INCR;
-    }
-    while (--w > 0);
+
+    } while ( --w > 0 );
 
     src_line += blit->src_pitch;
     dst_line += blit->dst_pitch;
-  }
-  while (--h > 0);
+
+  } while ( --h > 0 );
 
