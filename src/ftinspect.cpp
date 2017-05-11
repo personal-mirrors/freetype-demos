4b437c65174d125f8c3f1ddef196ec139002ae9b
Philipp Kerling <pkerling@casix.org>
2017-05-11 20:27:02 +0200

    [ftinspect] Make color tables and settings more readable.
    * src/ftinspect.cpp (MainGUI::setGraphicsDefaults): Use Qt
    constants and `qRgba'.

--- src/ftinspect.cpp
+++ src/ftinspect.cpp
@@ -1857,28 +1857,25 @@ MainGUI::setGraphicsDefaults()
 {
   // color tables (with suitable opacity values) for converting
   // FreeType's pixmaps to something Qt understands
-  monoColorTable.append(0x00FFFFFF);
-  monoColorTable.append(0xFF000000);
+  monoColorTable.append(QColor(Qt::transparent).rgba());
+  monoColorTable.append(QColor(Qt::black).rgba());
 
   for (int i = 0xFF; i >= 0; i--)
-    grayColorTable.append((0xFF - i) << 24
-                          | i << 16
-                          | i << 8
-                          | i);
+    grayColorTable.append(qRgba(i, i, i, 0xFF - i));
 
   // XXX make this user-configurable
 
-  axisPen.setColor(QColor(0, 0, 0, 255));        // black
+  axisPen.setColor(Qt::black);
   axisPen.setWidth(0);
   blueZonePen.setColor(QColor(64, 64, 255, 64)); // light blue
   blueZonePen.setWidth(0);
-  gridPen.setColor(QColor(192, 192, 192, 255));  // gray
+  gridPen.setColor(Qt::lightGray);
   gridPen.setWidth(0);
-  offPen.setColor(QColor(0, 128, 0, 255));       // dark green
+  offPen.setColor(Qt::darkGreen);
   offPen.setWidth(3);
-  onPen.setColor(QColor(255, 0, 0, 255));        // red
+  onPen.setColor(Qt::red);
   onPen.setWidth(3);
-  outlinePen.setColor(QColor(255, 0, 0, 255));   // red
+  outlinePen.setColor(Qt::red);
   outlinePen.setWidth(0);
   segmentPen.setColor(QColor(64, 255, 128, 64)); // light green
   segmentPen.setWidth(0);
