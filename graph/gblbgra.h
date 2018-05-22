89f1af01acfec275c54e6ac676cfd9d6226df4a2
Alexei Podtelezhnikov <apodtele@gmail.com>
2018-05-21 23:01:53 -0400

    * graph/gblbgra.h: Gamma-corrected variant (disabled).

--- graph/gblbgra.h
+++ graph/gblbgra.h
@@ -37,10 +37,28 @@
           unsigned int  back_g = (back >> 8) & 255;
           unsigned int  back_b = (back) & 255;
 
-
+#if 1     /* premultiplied blending without gamma correction */
           pix_r = (back_r * ba / 255 + pix_r);
           pix_g = (back_g * ba / 255 + pix_g);
           pix_b = (back_b * ba / 255 + pix_b);
+
+#else     /* gamma-corrected blending */
+          const unsigned char*   gamma_ramp_inv = blit->blender->gamma_ramp_inv;
+          const unsigned short*  gamma_ramp     = blit->blender->gamma_ramp;
+
+          back_r = gamma_ramp[back_r];
+          back_g = gamma_ramp[back_g];
+          back_b = gamma_ramp[back_b];
+
+          /* premultiplication undone */
+          pix_r = gamma_ramp[pix_r * 255 / a];
+          pix_g = gamma_ramp[pix_g * 255 / a];
+          pix_b = gamma_ramp[pix_b * 255 / a];
+
+          pix_r = gamma_ramp_inv[(back_r * ba + pix_r * a + 127) / 255];
+          pix_g = gamma_ramp_inv[(back_g * ba + pix_g * a + 127) / 255];
+          pix_b = gamma_ramp_inv[(back_b * ba + pix_b * a + 127) / 255];
+#endif
         }
 
         GDST_STOREC(dst,pix_r,pix_g,pix_b);
