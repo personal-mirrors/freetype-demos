e6040f12a39a83b0787e113621357446dc92feb3
Alexei Podtelezhnikov <apodtele@gmail.com>
2021-06-04 22:26:37 -0400

    * graph/gblender.c (gblender_dump_stats): Fix the lookup rate.

--- graph/gblender.c
+++ graph/gblender.c
@@ -413,17 +413,17 @@ GBLENDER_APIDEF( void )
 gblender_dump_stats( GBlender  blender )
 {
   printf( "GBlender cache statistics:\n" );
-  printf( "  Hit rate:    %.2f ( %ld out of %ld )\n",
+  printf( "  Hit rate:    %.2f%% ( %ld out of %ld )\n",
           100.0f * blender->stat_hits /
                    ( blender->stat_hits + blender->stat_lookups ),
           blender->stat_hits,
           blender->stat_hits + blender->stat_lookups );
 
-  printf( "  Lookup rate: %.2f ( %ld out of %ld )\n",
-          100.0f * blender->stat_lookups /
-                   ( blender->stat_lookups + blender->stat_keys ),
-          blender->stat_lookups,
-          blender->stat_keys + blender->stat_lookups );
+  printf( "  Lookup rate: %.2f%% ( %ld out of %ld )\n",
+          100.0f * ( blender->stat_lookups - blender->stat_keys ) /
+                   blender->stat_lookups,
+          blender->stat_lookups - blender->stat_keys,
+          blender->stat_lookups );
   printf( "  Keys used:   %ld\n  Caches full: %ld\n",
           blender->stat_keys, blender->stat_clears );
 }
