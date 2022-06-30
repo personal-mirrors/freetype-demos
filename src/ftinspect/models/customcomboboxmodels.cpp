4aea40d098112571d9611791e628bdf220f75374
Charlie Jiang <w@chariri.moe>
2022-06-30 22:28:26 +0800

    [ftinspect] Use custom `QAbstractListModel` instead of manually mapping.
    In `maingui.cpp`, mapping between `QComboBox` indices and actual FreeType
    values was done manually. The enabled statuses of individual ComboBox items
    were also maintained in `maingui.cpp`.
    
    In this commit, those code was moved to seperate custom classes
    implementating `QAbstractListModel`. All mapping was done there.
    Enable/Disable states was maintained in the classes as well.
    By doing this, the reason why some items were disabled was made more clear
    and explicit. Adding more items is easier.
    
    The `QComboBoxx` custom widget is no longer needed since disable flag can
    be provided by the custom model by overriding the `flags` function.
    
    `SimpleComboBoxModel` is a basic class for Key-Value-DisplayName mapping.
    Other models could extend this class if no complicated mapping (e.g.
    `HintingModeComboBoxModel`) is needed.
    
    * src/ftinspect/models/customcomboboxmodels.cpp,
      src/ftinspect/models/customcomboboxmodels.cpp: New files.
    
    * src/ftinspect/engine/engine.cpp, src/ftinspect/engine/engine.hpp:
      `Engine` now accepts "load target" value of certain anti-aliasing mode
      instead of the anti-aliasing enum value. The mapping was moved into
      `AntiAliasingComboBoxModel`.
    
    * src/ftinspect/maingui.cpp, src/ftinspect/maingui.hpp: Move all mappings
      and enabled/disabled statuses to separate models.
      Remove mapping `QHash`es. Use `QComboBox` instead of `QComboBoxx`.
    
    * src/ftinspect/meson.build, src/ftinspect/CMakeLists.txt: Updated.

---
+++ src/ftinspect/models/customcomboboxmodels.cpp
@@ -0,0 +1,292 @@
+// customcomboboxmodels.cpp
+
+// Copyright (C) 2022 by Charlie Jiang.
+
+
+#include "customcomboboxmodels.hpp"
+
+#include <QApplication>
+#include <QPalette>
+#include <freetype/ftdriver.h>
+#include <freetype/ftlcdfil.h>
+
+
+/////////////////////////////////////////////////////////////////////////////
+//
+// HintingModeComboBoxModel
+//
+/////////////////////////////////////////////////////////////////////////////
+
+HintingModeComboBoxModel::HintingModeComboBoxModel(QObject* parent)
+: QAbstractListModel(parent)
+{
+  items_[HintingMode_TrueType_v35] = {
+    HintingEngineType_TrueType,
+    HintingMode_TrueType_v35,
+    TT_INTERPRETER_VERSION_35,
+    false, false,
+    "TrueType v35"
+  };
+  items_[HintingMode_TrueType_v38] = {
+    HintingEngineType_TrueType,
+    HintingMode_TrueType_v38,
+    TT_INTERPRETER_VERSION_38,
+    false, false,
+    "TrueType v38"
+  };
+  items_[HintingMode_TrueType_v40] = {
+    HintingEngineType_TrueType,
+    HintingMode_TrueType_v40,
+    TT_INTERPRETER_VERSION_40,
+    false, false,
+    "TrueType v40"
+  };
+
+  items_[HintingMode_CFF_FreeType] = {
+    HintingEngineType_CFF,
+    HintingMode_CFF_FreeType,
+    FT_HINTING_FREETYPE,
+    false, false,
+    "CFF (FreeType)"
+  };
+  items_[HintingMode_CFF_Adobe] = {
+    HintingEngineType_CFF,
+    HintingMode_CFF_Adobe,
+    FT_HINTING_ADOBE,
+    false, false,
+    "CFF (Adobe)"
+  };
+}
+
+
+int
+HintingModeComboBoxModel::rowCount(const QModelIndex& parent) const
+{
+  return items_.size();
+}
+
+
+QVariant
+HintingModeComboBoxModel::data(const QModelIndex& index,
+  int role) const
+{
+  int r = index.row();
+  if (r < 0 || r >= items_.size())
+    return QVariant {};
+  HintingModeItem const& item = items_[r];
+
+  switch (role)
+  {
+  case Qt::DisplayRole:
+    return item.displayName;
+  case Qt::ForegroundRole:
+    if (item.enabled && item.supported)
+      return QVariant {};
+    else
+      return QApplication::palette().color(QPalette::Disabled, 
+                                           QPalette::Text);
+  default:
+    return QVariant {};
+  }
+}
+
+
+Qt::ItemFlags
+HintingModeComboBoxModel::flags(const QModelIndex& index) const
+{
+  int r = index.row();
+  if (r < 0 || r >= items_.size())
+    return Qt::ItemFlags {}; // not selectable, not enabled
+  HintingModeItem const& item = items_[r];
+
+  if (item.enabled && item.supported)
+    return Qt::ItemIsEnabled | Qt::ItemIsSelectable;
+
+  return Qt::ItemFlags {};
+}
+
+
+int
+HintingModeComboBoxModel::indexToValueForType(int index,
+                                              HintingEngineType type) const
+{
+  if (index < 0 || index >= items_.size())
+    return -1;
+  HintingModeItem const& item = items_[index];
+  if (!item.supported || !item.enabled || item.type != type)
+    return -1;
+  return item.value;
+}
+
+
+int
+HintingModeComboBoxModel::valueToIndexForType(int value,
+                                              HintingEngineType type) const
+{
+  for (const auto& item : items_)
+  {
+    if (item.type == type && item.value == value)
+      return item.key;
+  }
+  return -1;
+}
+
+
+int
+HintingModeComboBoxModel::indexToTTInterpreterVersion(int index) const
+{
+  return indexToValueForType(index, HintingEngineType_TrueType);
+}
+
+
+int
+HintingModeComboBoxModel::indexToCFFMode(int index) const
+{
+  return indexToValueForType(index, HintingEngineType_CFF);
+}
+
+
+int
+HintingModeComboBoxModel::cffModeToIndex(int mode) const
+{
+  return valueToIndexForType(mode, HintingEngineType_CFF);
+}
+
+
+int
+HintingModeComboBoxModel::ttInterpreterVersionToIndex(int version) const
+{
+  return valueToIndexForType(version, HintingEngineType_TrueType);
+}
+
+
+void
+HintingModeComboBoxModel::setSupportedModes(QList<int> supportedTTIVersions,
+                                            QList<int> supportedCFFModes)
+{
+  for (auto& item : items_)
+  {
+    if (item.type == HintingEngineType_TrueType)
+      item.supported = supportedTTIVersions.contains(item.value);
+    else if (item.type == HintingEngineType_CFF)
+      item.supported = supportedCFFModes.contains(item.value);
+    else
+      item.supported = false;
+  }
+}
+
+
+void
+HintingModeComboBoxModel::setCurrentEngineType(HintingEngineType type, 
+                                               bool tricky)
+{
+  for (auto& item : items_)
+    if (!tricky)
+      item.enabled = item.supported && item.type == type;
+    else
+      item.enabled = item.supported && item.key == HintingMode_TrueType_v35;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+//
+// LCDFilterComboBoxModel
+//
+/////////////////////////////////////////////////////////////////////////////
+
+
+LCDFilterComboBoxModel::LCDFilterComboBoxModel(QObject* parent)
+: QAbstractListModel(parent)
+{
+  items_[LCDFilter_Default] = {
+    FT_LCD_FILTER_DEFAULT,
+    "Default"
+  };
+  items_[LCDFilter_Light] = {
+    FT_LCD_FILTER_LIGHT,
+    "Light"
+  };
+  items_[LCDFilter_None] = {
+    FT_LCD_FILTER_NONE,
+    "None"
+  };
+  items_[LCDFilter_Legacy] = {
+    FT_LCD_FILTER_LEGACY,
+    "Legacy"
+  };
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+//
+// AntiAliasingComboBoxModel
+//
+/////////////////////////////////////////////////////////////////////////////
+
+
+AntiAliasingComboBoxModel::AntiAliasingComboBoxModel(QObject* parent)
+: QAbstractListModel(parent)
+{
+  items_[AntiAliasing_None] = {
+    {FT_LOAD_TARGET_MONO, FT_RENDER_MODE_MONO, false},
+    "None"
+  };
+  items_[AntiAliasing_Normal] = {
+    {FT_LOAD_TARGET_NORMAL, FT_RENDER_MODE_NORMAL, false},
+    "Normal"
+  };
+  items_[AntiAliasing_Light] = {
+    {FT_LOAD_TARGET_LIGHT, FT_RENDER_MODE_LIGHT, false},
+    "Light"
+  };
+  items_[AntiAliasing_Light_SubPixel] = {
+    {FT_LOAD_TARGET_LIGHT, FT_RENDER_MODE_LIGHT, false},
+    "Light (Sub Pixel)"
+  };
+  items_[AntiAliasing_LCD] = {
+    {FT_LOAD_TARGET_LCD, FT_RENDER_MODE_LCD, false},
+    "LCD (RGB)"
+  };
+  items_[AntiAliasing_LCD_BGR] = {
+    {FT_LOAD_TARGET_LCD, FT_RENDER_MODE_LCD, true},
+    "LCD (BGR)"
+  };
+  items_[AntiAliasing_LCD_Vertical] = {
+    {FT_LOAD_TARGET_LCD_V, FT_RENDER_MODE_LCD_V, false},
+    "LCD (vert. RGB)"
+  };
+  items_[AntiAliasing_LCD_Vertical_BGR] = {
+    {FT_LOAD_TARGET_LCD_V, FT_RENDER_MODE_LCD_V, true},
+    "LCD (vert. BGR)"
+  };
+
+  lightAntiAliasingEnabled_ = true;
+}
+
+
+QVariant
+AntiAliasingComboBoxModel::data(const QModelIndex& index,
+                                int role) const
+{
+  auto row = index.row();
+  if (role == Qt::ForegroundRole)
+    if ((row == AntiAliasing_Light || row == AntiAliasing_Light_SubPixel)
+        && !lightAntiAliasingEnabled_)
+      return QApplication::palette().color(QPalette::Disabled, 
+                                           QPalette::Text);
+  return SimpleComboBoxModelImpl::data(index, role);
+}
+
+
+Qt::ItemFlags
+AntiAliasingComboBoxModel::flags(const QModelIndex& index) const
+{
+  auto row = index.row();
+  if ((row == AntiAliasing_Light || row == AntiAliasing_Light_SubPixel)
+      && !lightAntiAliasingEnabled_)
+    return Qt::ItemFlags {};
+  return Qt::ItemIsSelectable | Qt::ItemIsEnabled;
+}
+
+
+// end of customcomboboxmodels.cpp
---
+++ src/ftinspect/models/customcomboboxmodels.hpp
@@ -0,0 +1,217 @@
+// customcomboboxmodels.hpp
+
+// Copyright (C) 2022 by Charlie Jiang.
+
+
+#pragma once
+
+#include <QAbstractListModel>
+#include <QHash>
+
+class HintingModeComboBoxModel
+: public QAbstractListModel
+{
+  Q_OBJECT
+public:
+  enum HintingEngineType : int;
+  enum HintingMode : int;
+  struct HintingModeItem
+  {
+    HintingEngineType type;
+    HintingMode key;
+    int value;
+    bool enabled;
+    bool supported;
+    QString displayName;
+  };
+
+  explicit HintingModeComboBoxModel(QObject* parent);
+  ~HintingModeComboBoxModel() override = default;
+
+  int rowCount(const QModelIndex& parent) const override;
+  QVariant data(const QModelIndex& index,
+                int role) const override;
+  Qt::ItemFlags flags(const QModelIndex& index) const override;
+
+  int indexToTTInterpreterVersion(int index) const;
+  int indexToCFFMode(int index) const;
+  int cffModeToIndex(int mode) const;
+  int ttInterpreterVersionToIndex(int version) const;
+
+  void setSupportedModes(QList<int> supportedTTIVersions,
+                         QList<int> supportedCFFModes);
+  void setCurrentEngineType(HintingEngineType type, bool tricky);
+
+private:
+  QHash<int, HintingModeItem> items_;
+
+  int indexToValueForType(int index,
+                          HintingEngineType type) const;
+  int valueToIndexForType(int value,
+                          HintingEngineType type) const;
+
+public:
+  // Note: Ensure related funcs are also changed when
+  // these enums are changed!
+  enum HintingEngineType : int
+  {
+    HintingEngineType_TrueType,
+    HintingEngineType_CFF
+  };
+
+  enum HintingMode : int
+  {
+    HintingMode_TrueType_v35 = 0,
+    HintingMode_TrueType_v38,
+    HintingMode_TrueType_v40,
+    HintingMode_CFF_FreeType,
+    HintingMode_CFF_Adobe
+  };
+};
+
+
+// A simple key-displayName-value model for QComboBox.
+template <class T>
+class SimpleComboBoxModelImpl
+{
+public:
+  struct ComboBoxItem
+  {
+    T value;
+    QString displayName;
+  };
+
+  SimpleComboBoxModelImpl() {}
+  virtual ~SimpleComboBoxModelImpl() = default;
+
+  virtual int
+  rowCount(const QModelIndex& parent) const
+  {
+    return items_.size();
+  }
+
+
+  virtual QVariant
+  data(const QModelIndex& index,
+                int role) const
+  {
+    if (role != Qt::DisplayRole)
+      return QVariant{};
+
+    int r = index.row();
+    if (r < 0 || r >= items_.size())
+      return QVariant{};
+    return items_[r].displayName;
+  }
+
+
+  virtual T
+  indexToValue(int index)
+  {
+    if (index < 0 || index >= items_.size())
+      return T();
+    return items_[index].value;
+  }
+
+protected:
+  QHash<int, ComboBoxItem> items_;
+};
+
+
+class LCDFilterComboBoxModel
+: public QAbstractListModel,
+  public SimpleComboBoxModelImpl<int>
+{
+  Q_OBJECT
+public:
+  enum LCDFilter : int;
+  struct LCDFilterItem
+  {
+    int value;
+    QString displayName;
+  };
+
+
+  explicit LCDFilterComboBoxModel(QObject* parent);
+  ~LCDFilterComboBoxModel() override = default;
+
+
+  int rowCount(const QModelIndex& parent) const override
+  {
+    return SimpleComboBoxModelImpl::rowCount(parent);
+  }
+
+
+  QVariant
+  data(const QModelIndex& index,
+       int role) const override
+  {
+    return SimpleComboBoxModelImpl::data(index, role);
+  }
+
+public:
+  enum LCDFilter : int
+  {
+    LCDFilter_Default,
+    LCDFilter_Light,
+    LCDFilter_None,
+    LCDFilter_Legacy
+  };
+};
+
+
+struct AASetting
+{
+  // No default value for braced init - No C++14, what a pain!
+  int loadFlag;
+  int renderMode;
+  bool isBGR;
+};
+
+
+class AntiAliasingComboBoxModel
+: public QAbstractListModel,
+  public SimpleComboBoxModelImpl<AASetting>
+{
+  Q_OBJECT
+public:
+  enum AntiAliasing : int;
+
+  explicit AntiAliasingComboBoxModel(QObject* parent);
+  ~AntiAliasingComboBoxModel() override = default;
+  
+  QVariant data(const QModelIndex& index,
+                int role) const;
+  Qt::ItemFlags flags(const QModelIndex& index) const;
+
+
+  int rowCount(const QModelIndex& parent) const override
+  {
+    return SimpleComboBoxModelImpl::rowCount(parent);
+  }
+
+
+  void setLightAntiAliasingEnabled(bool enabled)
+  {
+    lightAntiAliasingEnabled_ = enabled;
+  }
+
+private:
+  bool lightAntiAliasingEnabled_;
+
+public:
+  enum AntiAliasing : int
+  {
+    AntiAliasing_None,
+    AntiAliasing_Normal,
+    AntiAliasing_Light,
+    AntiAliasing_Light_SubPixel,
+    AntiAliasing_LCD,
+    AntiAliasing_LCD_BGR,
+    AntiAliasing_LCD_Vertical,
+    AntiAliasing_LCD_Vertical_BGR
+  };
+};
+
+
+// end of customcomboboxmodels.hpp
