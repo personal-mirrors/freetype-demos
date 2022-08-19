ef14bfd21376ff5c9c7ab4ac0f95c374f83cc7b5
Charlie Jiang <w@chariri.moe>
2022-08-19 16:54:23 +0800

    * src/ftinspect/engine/engine.cpp: Support LCD rendering.
    It was a great decision to refactor the color table part.

--- src/ftinspect/engine/engine.cpp
+++ src/ftinspect/engine/engine.cpp
@@ -987,18 +987,70 @@ Engine::calculateForegroundTable()
 void
 convertLCDToARGB(FT_Bitmap& bitmap,
                  QImage& image,
-                 bool isBGR)
+                 bool isBGR,
+                 QVector<QRgb>& colorTable)
 {
-  // TODO to be implemented
+  int height = bitmap.rows;
+  int width = bitmap.width / 3;
+  int width3 = bitmap.width;
+
+  unsigned char* srcPtr = bitmap.buffer;
+  unsigned* dstPtr = reinterpret_cast<unsigned*>(image.bits());
+
+  int offR = !isBGR ? 0 : 2;
+  int offG = 1;
+  int offB = isBGR ? 0 : 2;
+  for (int i = 0; i < height; i++)
+  {
+    for (int j = 0; j < width3; j += 3)
+    {
+      unsigned char ar = srcPtr[j + offR];
+      unsigned char ag = srcPtr[j + offG];
+      unsigned char ab = srcPtr[j + offB];
+      unsigned dr = colorTable[ar] & 0xFF;
+      unsigned dg = colorTable[ag] & 0xFF;
+      unsigned db = colorTable[ab] & 0xFF;
+      *dstPtr = (0xFFu << 24) | (dr << 16) | (dg << 8) | db;
+      dstPtr++;
+    }
+    srcPtr += bitmap.pitch;
+    dstPtr += image.bytesPerLine() / 4 - width; // skip blank area
+  }
 }
 
 
 void
 convertLCDVToARGB(FT_Bitmap& bitmap,
                   QImage& image,
-                  bool isBGR)
+                  bool isBGR,
+                  QVector<QRgb>& colorTable)
 {
-  // TODO to be implemented
+  int height = bitmap.rows / 3;
+  int width = bitmap.width;
+  int srcPitch = bitmap.pitch;
+
+  unsigned char* srcPtr = bitmap.buffer;
+  unsigned* dstPtr = reinterpret_cast<unsigned*>(image.bits());
+
+  int offR = !isBGR ? 0 : 2 * srcPitch;
+  int offG = srcPitch;
+  int offB = isBGR ? 0 : 2 * srcPitch;
+  for (int i = 0; i < height; i++)
+  {
+    for (int j = 0; j < width; j++)
+    {
+      unsigned char ar = srcPtr[j + offR];
+      unsigned char ag = srcPtr[j + offG];
+      unsigned char ab = srcPtr[j + offB];
+      unsigned dr = colorTable[ar] & 0xFF;
+      unsigned dg = colorTable[ag] & 0xFF;
+      unsigned db = colorTable[ab] & 0xFF;
+      *dstPtr = (0xFFu << 24) | (dr << 16) | (dg << 8) | db;
+      dstPtr++;
+    }
+    srcPtr += 3ull * srcPitch;                  // move 3 lines
+    dstPtr += image.bytesPerLine() / 4 - width; // skip blank area
+  }
 }
 
 
@@ -1076,11 +1128,11 @@ Engine::convertBitmapToQImage(FT_Bitmap* src)
     break;
   case FT_PIXEL_MODE_LCD:;
     result = new QImage(width, height, format);
-    convertLCDToARGB(bmap, *result, lcdUsesBGR_);
+    convertLCDToARGB(bmap, *result, lcdUsesBGR_, foregroundTable_);
     break;
   case FT_PIXEL_MODE_LCD_V:;
     result = new QImage(width, height, format);
-    convertLCDVToARGB(bmap, *result, lcdUsesBGR_);
+    convertLCDVToARGB(bmap, *result, lcdUsesBGR_, foregroundTable_);
     break;
   }
 
