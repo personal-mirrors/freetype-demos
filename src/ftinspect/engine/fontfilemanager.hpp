7ff9de6dd24c3fd4b3c716fbd531cc9b1a4fb652
Charlie Jiang <w@chariri.moe>
2022-09-02 11:43:21 +0800

    [ftinspect] Move out font file managing/watching out to a new class.
    Introduce the feature that rejects invalid files.
    
    * src/ftinspect/maingui.hpp, src/ftinspect/maingui.cpp: Move `fontList`,
      `fontWatcher`, `timer` and other code related to font file managing to
      new class `FontFileManager`.
    
    * src/ftinspect/engine.hpp, src/ftinspect/engine.cpp:
      `Engine` class now holds `FontFileManager`.
      Also, make `MainGUI` open and close fonts indirectly via `Engine`.
    
    * src/ftinspect/fontfilemanager.hpp, src/ftinspect/fontfilemanager.cpp:
      New files.
    
    * src/ftinspect/CMakeLists.txt, src/ftinspect/meson.build: Updated.

---
+++ src/ftinspect/engine/fontfilemanager.hpp
@@ -0,0 +1,53 @@
+// fontfilemanager.hpp
+
+// Copyright (C) 2022 by Charlie Jiang.
+
+#pragma once
+
+#include <QObject>
+#include <QList>
+#include <QFileSystemWatcher>
+#include <QTimer>
+#include <QFileInfo>
+
+#include <freetype/freetype.h>
+
+
+// Class to manage all opened font files, as well as monitoring local file
+// change.
+
+class Engine;
+class FontFileManager
+: public QObject
+{
+  Q_OBJECT
+public:
+  FontFileManager(Engine* engine);
+  ~FontFileManager() override = default;
+
+  int size();
+  void append(QStringList const& newFileNames, bool alertNotExist = false);
+  void remove(int index);
+
+  QFileInfo& operator[](int index);
+  void updateWatching(int index);
+  void timerStart();
+  
+signals:
+  void currentFileChanged();
+
+private slots:
+  void onTimerFire();
+  void onWatcherFire();
+
+private:
+  Engine* engine;
+  QList<QFileInfo> fontFileNameList;
+  QFileSystemWatcher* fontWatcher;
+  QTimer* watchTimer;
+
+  FT_Error validateFontFile(QString const& fileName);
+};
+
+
+// end of fontfilemanager.hpp
