2ece842dd580ccf4d984995969e867117cf0ba92
Charlie Jiang <w@chariri.moe>
2022-09-03 22:10:51 +0800

    [ftinspect] Rewrite `MainGUI`.
    Note: This commit compiles, but the main view is removed, so you will get a
    blank right panel. The singular view will be added back with major changes
    in the next commit.
    
    This commit mainly introduces below changes:
    
    1. The original `MainGUI` contains almost all GUI elements. Most of them are
       taken out to modular components. The current `MainGUI` serves only as
       a skeleton provides coordinate between components, greatly shortened.
       It also provides some auxiliary code such as calling to file chooser.
    2. The left panel is moved to `SettingPanel` class. The current
       `settingpanel.[ch]pp` are directly modified from the latest code, so they
       contains some options not implemented in the `Engine`.
       Structural code that is only added accompanying later change is removed,
       such as the comparator mode. Such code will be added back with the
       related feature. However, code for unimplemented options are simply
       commented out.
    3. The main part is transformed into a tabbed view. The original code is
       removed. Refactored, it will be added back in the next commit.
    4. The navigation buttons (Next/Prev Font/Face/NI) are changed into the new
       triplet-selector, which is a major UI improvement.
    
    * src/ftinspect/maingui.cpp, src/ftinspect/maingui.hpp: As described.
    
    * src/ftinspect/panels/abstracttab.hpp: Add `AbstractTab` which is an
      interface for all tabs listening for font reloading and repainting.
    
    * src/ftinspect/panels/settingpanel.cpp,
      src/ftinspect/panels/settingpanel.hpp:
      As described, this is the left panel. This requires intensive reviewing
      since many bugs had rooted here.
    
    * src/ftinspect/widgets/tripletselector.cpp,
      src/ftinspect/widgets/tripletselector.hpp:
      As described, this is the triplet (Font/Subface/NI) selector.
      This component is also responsible for repopulating triplet information
      and keep up with the font file change (i.e. the
      `FontFileManager::currentFileChanged` event is captured here).
    
    * src/ftinspect/engine/engine.cpp, src/ftinspect/engine/engine.hpp:
      Add `fontValid` and `namedInstanceName` since `TripletSelector` requires
      them. However, they'll subject to change later.
    
    * src/ftinspect/ftinspect.cpp: Remove call to `MainGUI::setDefaults`.
    
    * src/ftinspect/CMakeLists.txt, src/ftinspect/meson.build: Updated.

---
+++ src/ftinspect/panels/abstracttab.hpp
@@ -0,0 +1,20 @@
+// abstracttab.hpp
+
+// Copyright (C) 2022 by Charlie Jiang.
+
+#pragma once
+
+// This is an pure abstract interface for a ftinspect "tab".
+// The interface itself does not inherit from `QWidget`, but should be used as
+// the second base class.
+class AbstractTab
+{
+public:
+  virtual ~AbstractTab() = default; // must be `virtual` for `dynamic_cast`
+  
+  virtual void repaintGlyph() = 0;
+  virtual void reloadFont() = 0;
+};
+
+
+// end of abstracttab.hpp
---
+++ src/ftinspect/widgets/tripletselector.hpp
@@ -0,0 +1,67 @@
+// QPushButton.hpp
+
+// Copyright (C) 2022 by Charlie Jiang.
+
+#pragma once
+
+#include <vector>
+#include <QWidget>
+#include <QComboBox>
+#include <QPushButton>
+#include <QToolButton>
+#include <QBoxLayout>
+
+class Engine;
+class TripletSelector
+: public QWidget
+{
+  Q_OBJECT
+
+public:
+  TripletSelector(QWidget* parent,
+                  Engine* engine);
+  ~TripletSelector() override;
+
+  void repopulateFonts();
+  void closeCurrentFont();
+
+signals:
+  void tripletChanged();
+
+private:
+  Engine* engine_;
+
+  QComboBox* fontComboBox_;
+  QComboBox* faceComboBox_;
+  QComboBox* niComboBox_;
+
+  QToolButton* closeFontButton_;
+
+  QToolButton* fontUpButton_;
+  QToolButton* fontDownButton_;
+  QToolButton* faceUpButton_;
+  QToolButton* faceDownButton_;
+  QToolButton* niUpButton_;
+  QToolButton* niDownButton_;
+
+  QHBoxLayout* layout_;
+
+  void checkButtons();
+  void watchCurrentFont();
+
+  void createLayout();
+  void createConnections();
+
+  void repopulateFaces(bool fontSwitched = true);
+  void repopulateNamedInstances(bool fontSwitched = true);
+  void updateFont();
+  void updateFace();
+  void updateNI();
+  void loadTriplet();
+
+  static void nextComboBoxItem(QComboBox* c);
+  static void previousComboBoxItem(QComboBox* c);
+};
+
+
+// end of QPushButton.hpp
